8fd3155331067d9a3f544277d59ef2583324f44f
utsavipatil <17bee076@nirmauni.ac.in>
2026-03-14 12:55:45 +0530

    .gitignore updated

---
+++ src/test/java/com/utsavi/bookmyshow/BoomyshowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utsavi.bookmyshow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoomyshowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
